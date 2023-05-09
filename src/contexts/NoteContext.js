3e27ec92aa10cf79948496e3eac797bbeac28b7c
tanmayidev <tanmayi.d.99@gmail.com>
2023-05-09 16:02:45 +0530

    edit functionality

--- src/contexts/NoteContext.js
+++ src/contexts/NoteContext.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
-
 const NotesContext = React.createContext();
-
 const NotesContextProvider = ({ children }) => {
   const [notes, setNotes] = useState([]);
-
   // addNote function
   const addNote = ({ title, content }) => {
     const newNote = {
@@ -16,11 +13,27 @@ const NotesContextProvider = ({ children }) => {
     setNotes([...notes, newNote]);
   };
 
+  //deleteNote function
+  const deleteNote = (noteId) => {
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+    setNotes(updatedNotes);
+  };
+
+  // updateNote
+  const updateNote = (updatedNote) => {
+    const updatedNotes = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+  };
+
   return (
-    <NotesContext.Provider value={{ notes, addNote }}>
+    <NotesContext.Provider value={{ notes, addNote, deleteNote, updateNote }}>
       {children}
     </NotesContext.Provider>
   );
 };
-
 export { NotesContext, NotesContextProvider };
